7f59f7a6fbc3f57ab235af6e0ea6bb307b40e58e
NiallMac <niall.maccrann@gmail.com>
2025-11-07 06:15:05 -0800

    tidied up run_auto.sh

--- run_auto.sh
+++ run_auto.sh
@@ -18,12 +18,8 @@ do
     echo $est_maps
     echo `date`
 
-    #run the auto measurement (multiple processes help for the cross-estimator)
-    cmd="srun -u -l -n 8 python bin/run_auto.py ${outdir}/output_${est_maps}_${tag} -c $config --use_mpi True --est_maps $est_maps"
-    #echo $cmd
-    #$cmd
-
-    #rdn0 and meanfield (same script but now use more processees since we're iterating over sims)
+    #run the auto measurement (multiple processes help for the cross-estimator),
+    #rdn0 and meanfield
     cmd="srun -u -l -n 32 python bin/run_auto.py ${outdir}/output_${est_maps}_${tag} -c $config --use_mpi True --do_rdn0 True --do_meanfield True --est_maps $est_maps"
     echo $cmd
     $cmd
